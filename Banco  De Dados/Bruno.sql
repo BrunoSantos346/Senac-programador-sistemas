5375a10b239c94d0d9a2d236596e9a049524561a
BRUNO LUIS FERREIRA DOS SANTOS <bruno.lfsantos1@senacsp.edu.br>
2025-04-02 22:06:02 -0300

    Exercício 02

--- Banco  De Dados/Bruno.sql
+++ Banco  De Dados/Bruno.sql
@@ -1,91 +1,104 @@
-CREATE TABLE IF NOT EXISTS empregado(
+CREATE TABLE cliente(
 id INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(100),
+nome VARCHAR(100) NOT NULL,
 idade INT NOT NULL,
-id_departamento INT,
-FOREIGN KEY (id_departamento) REFERENCES departamento(id),
-salario DECIMAL NOT NULL
+cidade VARCHAR(100) NOT NULL,
+saldo DECIMAL NOT NULL
 );
 
 
 
-INSERT INTO empregado (
-nome, idade, id_departamento, salario
-)VALUES
- ('João',30,'1',50000),
- ('Sarah',28,'2',60000),
- ('Miguel',35,'3',55000),
- ('Ana', 27, '2', 62000);
- 
- SELECT * FROM empregado 
- WHERE departamento 
- LIKE 'Ti';
- 
- SELECT nome, salario FROM empregado 
- WHERE salario > 55000;
- 
- SELECT * FROM empregado 
- ORDER BY idade DESC;
- 
- 
- -- Exercício 2 --
+INSERT INTO cliente(
+nome, 
+idade, 
+cidade, 
+saldo 
+)
+
+VALUES
+('Carlos', '45', 'São Paulo', 2500.00),
+('Mariana', '32', 'Rio de Janeiro', '3200.50'),
+('Pedro',  '27', 'Belo Horizonte', '1500.75'),
+('Fernada', '38', 'Curitiba', 4200.00);
+
+-- Exercício 1 --
+
+SELECT nome FROM cliente 
+WHERE cidade = 'Rio de janeiro';
+
+SELECT nome, saldo FROM cliente
+WHERE  saldo > '2000.00';
+
+SELECT nome, idade FROM cliente
+WHERE idade > '30';
 
 
-SELECT * FROM empregado
-WHERE idade 
-IN (28, 35);
+-- Exercício 2 --
 
-SELECT * FROM empregado
+SELECT nome, idade FROM cliente
+WHERE idade BETWEEN 25 AND 40;
+
+SELECT * FROM cliente 
 WHERE nome 
-LIKE '%M%';
+LIKE '%F%';
 
-SELECT * FROM empregado
-WHERE departamento != 'RH';
+SELECT * FROM cliente 
+WHERE cidade NOT IN('São Paulo', 'Curitiba');
+
+-- Exercício 3 --
+
+CREATE TABLE pedido(
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+cliente_id INT NOT NULL,
+valor DECIMAL(10,2) NOT NULL,
+data_pedido DATETIME
+);
+
+INSERT INTO pedido(
+cliente_id,
+valor,
+data_pedido
+)
+VALUES
+('1', 500.00, '2024-03-10'),
+('2', 1200.00, '2024'-03-12),
+('3', 300.50, '2024-03-15'),
+('1', 800.00, '2024-08-18');
+
+SELECT AVG(valor) FROM pedido
+WHERE cliente_id;
+
+SELECT cliente_id, valor FROM pedido
+GROUP BY valor;
 
 
- -- Excercício 3 --
- 
- 
- SELECT COUNT(*) FROM empregado 
- WHERE departamento = 'Ti';
- 
- SELECT COUNT(*) FROM empregado
- WHERE departamento = 'RH';
- 
-  SELECT COUNT(*) FROM empregado
- WHERE departamento = 'Vendas';
- 
- SELECT AVG(salario) FROM empregado
- WHERE departamento = 'Ti';
- 
- SELECT SUM(salario) FROM empregado
- WHERE departamento = 'Vendas';
- 
- 
- -- Exercício 4 --
- 
- SELECT empregado.nome, departamento.nome FROM empregado
- INNER JOIN departamento 
- ON empregado.id_departamento = departamento.id;
- 
- SELECT *
- FROM departamento
- LEFT JOIN empregado
- ON departamento.id = empregado.id;
- 
- -- Exercício 5 --
- 
-SELECT nome, salario
-FROM empregado
-WHERE salario > (SELECT AVG(salario) FROM empregado);
- 
- 
- 
- 
- 
+
+-- Exercício 4 --
+
+SELECT pedido.id, cliente.nome, valor
+FROM cliente
+INNER JOIN pedido ON cliente.id = pedido.cliente_id;
+
 
  
  
- 
- 
+SELECT nome, valor
+FROM cliente
+LEFT JOIN pedido ON pedido.cliente_id  = cliente_id;
+
+-- Exercício 5 --
+
+SELECT nome, valor
+FROM pedido
+WHERE cliente_id = (SELECT AVG(valor) FROM pedido WHERE valor > '1000.00');
+
+
+
+
+
+
+
+
+
 
